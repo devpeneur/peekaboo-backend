48fbdb44d2ebaf6a6f77a23ff7e3d379ff6e9d0e
Piyush Dhall <contact@piyush.ovh>
2024-05-19 06:53:22 +0530

    add models

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -5,6 +5,8 @@ rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute
 
 cp -r ./dist ./.amplify-hosting/compute/default
+cp -r ./models ./.amplify-hosting/compute/default
+
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r public ./.amplify-hosting/static
